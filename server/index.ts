61262729f2d5157b4ff7c2efe96751a6054a9ca2
Berk Koca <berk1937@gmail.com>
2022-03-04 10:44:05 +0300

    Main router added

--- server/index.ts
+++ server/index.ts
@@ -3,12 +3,14 @@ dotenv.config({ path: ".env" });
 
 import express, { Application, json } from "express";
 import cors from "cors";
+import router from "./routes";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(json());
+app.use("/api", router);
 
 async function start(): Promise<void> {
     app.listen(port, () => console.log(`Server listening on port ${port}`));
