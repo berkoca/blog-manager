2d540989093a459f3c080f35c6788c80166e57f9
Berk Koca <berk1937@gmail.com>
2022-03-05 16:43:51 +0300

    env example added

--- server/routes/authentication.ts
+++ server/routes/authentication.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { AuthenticationController } from "../controllers/AuthenticationController";
 
-const controller = new AuthenticationController();
+const controller: AuthenticationController = new AuthenticationController();
 const authentication_router: Router = Router();
 
 authentication_router.post("/login", controller.login);
--- server/routes/post.ts
+++ server/routes/post.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { PostController } from "../controllers/PostController";
 
+const controller: PostController = new PostController();
 const post_router: Router = Router();
+
+post_router.get("/", controller.getPosts);
+post_router.post("/", controller.createPost);
 
 export default post_router;
