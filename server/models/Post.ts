05f45f7598b540aa4ad9d1834cd8b60dad28917d
Berk Koca <berk1937@gmail.com>
2022-03-05 15:34:40 +0300

    User and Post models added

---
+++ server/models/Post.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+    image_url: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    category_tags: [String],
+    user_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "user"
+    }
+});
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
